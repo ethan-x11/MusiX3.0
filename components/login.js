843ba61b377fc39f81b252e4a43ea0c88b4f01df
ethan-x11 <subhamdutta96740@gmail.com>
2023-01-08 03:29:56 +0530

    added basic homepage structure

--- components/login.js
+++ components/login.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import { useWallet } from '@solana/wallet-adapter-react'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { Wallet } from '@project-serum/anchor'
+import Payments from './payments'
 
 const styles = {
     loginPage: `w-screen h-screen flex justify-center flex-col items-center bg-gray-200`,
-    text: `text-4xl font-bold text-gray-700 padding-bottom-100`
+    text: `text-4xl font-bold text-gray-700 mb-10`
 }
 
 const Login = () => {
-  return (
+
+    const wallet = useWallet()
+
+    if(wallet.connected) return (<Payments />)
+
+    return (
     <div className={styles.loginPage}>
         <p className={styles.text}> Login using Phantom Wallet to Access </p>
-       <WalletMultiButton />
+        <WalletMultiButton />
     </div>
-  )
+    )
 }
 
 export default Login
