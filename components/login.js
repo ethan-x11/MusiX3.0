fb3ab102c8b361ad962f1d745faf51366bbc6675
ethan-x11 <subhamdutta96740@gmail.com>
2023-01-08 00:28:01 +0530

    wallet connection provider setup

--- components/login.js
+++ components/login.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 
 const styles = {
     loginPage: `w-screen h-screen flex justify-center flex-col items-center bg-gray-200`,
@@ -8,8 +9,8 @@ const styles = {
 const Login = () => {
   return (
     <div className={styles.loginPage}>
-        <p className={styles.text}> Login to Access </p>
-        
+        <p className={styles.text}> Login using Phantom Wallet to Access </p>
+        { /* <WalletMultiButton /> */}
     </div>
   )
 }
---
+++ utils/const.js
@@ -0,0 +1,4 @@
+import { PublicKey } from "@solana/web3.js";
+
+
+export const SOLANA_HOST = "https://muddy-shy-dawn.solana-devnet.quiknode.pro/5b638e723dd3df716748cb5432458a2e21e53e05/";
